539ebb1d0049295b7a7830d1b6ed7aaefb727a9c
ifiyemiO <ifiyemi.owi@keyin.com>
2024-11-13 15:06:22 -0330

    DSA

---
+++ src/MidSprint/User.java
@@ -0,0 +1,45 @@
+package MidSprint;
+
+public class User {
+    private String name;
+    private TaskList taskList;
+
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addTask(String description) {
+        taskList.addTask(description);
+    }
+
+    public void markTaskAsCompleted(String description) {
+        taskList.markTaskAsCompleted(description);
+    }
+
+    public void printTasks() {
+        System.out.println("Tasks for " + name + ":");
+        taskList.printTasks();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
